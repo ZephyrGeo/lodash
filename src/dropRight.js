1ff496c1be3bef75f7881c17b6e853309f40458a
ZephyrGeo <1255356291@qq.com>
2023-07-10 12:33:54 -0400

    update

--- src/dropRight.js
+++ src/dropRight.js
@@ -2,13 +2,8 @@ import slice from './slice.js'
 
 const dropRight = (arr, n = 1) => {
     const length = arr == null ? 0 : arr.length
-    return slice(arr, 0, length - n)
+    n = length - n
+    return length ? slice(arr, 0, n < 0 ? 0 : n) : []
 }
 
 export default dropRight
-
-const arr = [1, 2, 3]
-console.log(dropRight([1, 2, 3]))
-console.log(dropRight([1, 2, 3], 2))
-console.log(dropRight([1, 2, 3], 5))
-console.log(dropRight([1, 2, 3], 0))
