7d62ec6fbf7adff2664f00c9404644caa3abc10a
ZephyrGeo <1255356291@qq.com>
2023-07-11 10:10:00 -0400

    update

--- src/dropRight.js
+++ src/dropRight.js
@@ -2,7 +2,7 @@ import slice from './slice.js'
 
 const dropRight = (arr, n = 1) => {
     const length = arr == null ? 0 : arr.length
-    n = length - n
+    n = Math.ceil(length - n)
     return length ? slice(arr, 0, n < 0 ? 0 : n) : []
 }
 
