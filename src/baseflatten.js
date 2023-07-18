a903000736e3a59781437eee0f979e5eca98ac9a
ZephyrGeo <1255356291@qq.com>
2023-07-18 07:31:46 -0400

    update

---
+++ src/baseflatten.js
@@ -0,0 +1,29 @@
+const baseFlatten = (arr, depth, predicate, isStrict, result) => {
+    const spreadableSymbol = Symbol.isConcatSpreadable
+    function isFlattenable(val) {
+        return Array.isArray(val) || !!(val && val[spreadableSymbol])
+    }
+
+    predicate || (predicate = isFlattenable)
+    result || (result = [])
+
+    if (arr == null) return result
+
+    for (const val of arr) {
+        if (depth > 0 && predicate(val)) {
+            if (depth > 1) {
+                baseFlatten(val, depth - 1, predicate, isStrict, result)
+            } else {
+                result.push(...val)
+            }
+        } else if (!isStrict) {
+            result[result.length] = val
+        }
+    }
+    return result
+}
+
+export default baseFlatten
+
+const arr = [1, [[[5]]], 6]
+console.log(baseFlatten(arr, 3))
