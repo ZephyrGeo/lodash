4635963d18efaae3a52eb08505c42d7563f7f2a7
ZephyrGeo <1255356291@qq.com>
2023-07-19 00:21:15 -0400

    update

--- src/concat.js
+++ src/concat.js
@@ -1,1 +1,9 @@
-const concat = (arr, values) => {}
+import slice from './slice.js'
+
+const concat = (arr, values) => {
+    const result = slice(arr)
+    result.push(...values)
+    return result
+}
+
+export default concat
