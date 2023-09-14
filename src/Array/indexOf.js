f694409f44fe0d777b736547fbfa8b089e74ca7b
ZephyrGeo <1255356291@qq.com>
2023-09-14 09:21:05 -0400

    update

--- src/Array/indexOf.js
+++ src/Array/indexOf.js
@@ -1,1 +1,11 @@
-const indexOf = (arr, value, fromIndex = 0) => {}
+const indexOf = (arr, value, fromIndex = 0) => {
+    const length = arr == null ? 0 : arr.length
+    fromIndex = fromIndex < 0 ? length + fromIndex : fromIndex
+
+    for (let i = fromIndex; i < length; i++) {
+        if (arr[i] === value) return i
+    }
+    return -1
+}
+
+export default indexOf
