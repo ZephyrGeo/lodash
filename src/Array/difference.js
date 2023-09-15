cc9337076aacab70a730a9311391a91e7ecdbc77
ZephyrGeo <1255356291@qq.com>
2023-09-15 12:07:33 -0400

    update

--- src/Array/difference.js
+++ src/Array/difference.js
@@ -1,8 +1,8 @@
-// use filter and includes function
 const difference = (arr, val) => {
     return arr.filter((item) => !val.includes(item))
 }
 
 export default difference
 
-console.log(difference([3, 2, 1], [4, 2]))
+// difference([3, 2, 1], [4, 2])
+// => [3,1]
