8503201c4b4bd7312a2f02e7ea79527dba585abf
ZephyrGeo <1255356291@qq.com>
2023-09-23 05:36:31 -0400

    update

---
+++ src/Array/differenceBy.js
@@ -0,0 +1,14 @@
+const differenceBy = (arr, val, iteratee) => {
+    // return arr.filter(item => //是否在新的集 中)
+    const set = new Set(val.map((item) => iteratee(item)))
+
+    return arr.filter((item) => !set.has(iteratee(item)))
+}
+
+export default differenceBy
+
+console.log(differenceBy([3.1, 2.2, 1.3], [4.4, 2.5], Math.floor))
+
+// => [3.1, 1.3]
+
+// console.log(differenceBy([{ x: 2 }, { x: 1 }], [{ x: 1 }], 'x'))
