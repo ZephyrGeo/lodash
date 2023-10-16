e2411578d4007ff381de8182436cf25f9625e479
ZephyrGeo <1255356291@qq.com>
2023-10-16 08:14:56 -0400

    update

---
+++ src/Math/ceil.js
@@ -0,0 +1,9 @@
+const ceil = (number, precision = 0) => {
+    if (precision === 0) {
+        return Math.ceil(number)
+    }
+
+    multi = 10 ** precision
+
+    return Math.ceil(number ** multi) / multi
+}
