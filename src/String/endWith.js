6813f6dfa77fb22e38a76bf5de0a453f7cdc91d2
ZephyrGeo <1255356291@qq.com>
2023-10-17 12:07:55 -0400

    upda te

---
+++ src/String/endWith.js
@@ -0,0 +1,20 @@
+const endsWith = (string = '', target, position = string.length) => {
+    position = position < string.length ? position : string.length
+    const new_string = string.slice(0, position)
+
+    const new_str_len = new_string.length
+    const tar_len = target.length
+
+    // 从后向前比较
+    for (let i = tar_len - 1; i >= 0; i--) {
+        if (target[i] !== new_string[i + new_str_len - tar_len]) return false
+    }
+    return true
+}
+
+console.log(endsWith('abc', 'c')) // => true
+console.log(endsWith('abc', 'a')) // => false
+console.log(endsWith('abc', 'b', 2)) // => true
+console.log(endsWith('abc', 'bc', 2)) // => false
+console.log(endsWith('abc', 'ab', 2)) // => true
+console.log(endsWith('abc', 'ab', 3)) // => false
