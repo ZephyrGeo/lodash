f96d9b115a462da1025c703c18c485f5c835dcd0
ZephyrGeo <1255356291@qq.com>
2023-10-31 09:43:09 -0400

    update

--- src/Object/findKey.js
+++ src/Object/findKey.js
@@ -1,11 +1,28 @@
 const findKey = (obj, pred) => {
+    if (Array.isArray(pred)) {
+        for (const key in obj) {
+            // barney , { 'age': 36, 'active': true }
+            for (const name in obj[key]) {
+                if (name === pred[0] && obj[key][name] === pred[1]) {
+                    return key
+                }
+            }
+        }
+        return false
+    }
+
     if (typeof pred === 'string') {
         return findKey(obj, (el) => el[pred])
     }
-    if (Array.isArray(pred)) {
-        const res = Object.fromEntries(pred)
-        return findKey(obj, res)
+
+    if (typeof pred === 'object') {
+        for (const key in obj) {
+            // console.log(key, obj[key], pred)
+            if (JSON.stringify(obj[key]) === JSON.stringify(pred)) return key
+        }
+        return false
     }
+
     for (const key in obj) {
         if (obj.hasOwnProperty(key) && pred(obj[key])) {
             return key
@@ -14,21 +31,24 @@ const findKey = (obj, pred) => {
     return false
 }
 
-const users = {
-    barney: { age: 36, active: true },
-    fred: { age: 40, active: false },
-    pebbles: { age: 1, active: true },
-}
+// const users = {
+//     barney: { age: 36, active: true },
+//     fred: { age: 40, active: false },
+//     pebbles: { age: 1, active: true },
+// }
 
-console.log(
-    findKey(users, function (o) {
-        return o.age < 40
-    })
-)
+// console.log(
+//     findKey(users, function (o) {
+//         return o.age < 40
+//     })
+// )
+// 'barney'
 
-console.log(findKey(users, ['active', false]))
+// console.log(findKey(users, { age: 1, active: true }))
+// => 'pebbles'
 
-console.log(findKey(users, 'active'))
+// console.log(findKey(users, ['active', false]))
+// => 'fred'
 
-const arr = [1, 23]
-console.log(Array.isArray(arr))
+// console.log(findKey(users, 'active'))
+// => 'barney'
